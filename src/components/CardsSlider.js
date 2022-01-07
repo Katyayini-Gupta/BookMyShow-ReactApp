ede9feb6187e0363279b90cd3e2477ecbed28ec0
Katyayini-Gupta <guptakatyayini2002@gmail.com>
2022-01-07 17:49:11 +0530

    first commit

---
+++ src/components/CardsSlider.js
@@ -0,0 +1,176 @@
+import Carousel from 'react-bootstrap/Carousel'
+import Card from 'react-bootstrap/Card'
+import CardGroup from 'react-bootstrap/CardGroup';
+
+function CardsSlider() {
+  return (
+    <div style={{marginBottom: "5%", background: "grey", padding: "5%"}}>
+      <Carousel fade variant="dark" indicators={false}>
+        <Carousel.Item>
+          <CardGroup style={{display: "flex", justifyContent: "space-around", gap: "30px"}}>
+
+            <Card className="bg-dark text-white" style={{height: "250px", width: "250px"}}>
+              <Card.Img src="./logo192.png" alt="Card image" style={{height: "250px", width: "250px"}}/>
+              <Card.ImgOverlay>
+                <Card.Title>Card title</Card.Title>
+                <Card.Text>
+                  This is a wider card with supporting text below as a natural lead-in to
+                  additional content. This content is a little bit longer.
+                </Card.Text>
+                <Card.Text>Last updated 3 mins ago</Card.Text>
+              </Card.ImgOverlay>
+            </Card>
+
+            <Card className="bg-dark text-white" style={{height: "250px", width: "250px"}}>
+              <Card.Img src="./logo192.png" alt="Card image" style={{height: "250px", width: "250px"}}/>
+              <Card.ImgOverlay>
+                <Card.Title>Card title</Card.Title>
+                <Card.Text>
+                  This is a wider card with supporting text below as a natural lead-in to
+                  additional content. This content is a little bit longer.
+                </Card.Text>
+                <Card.Text>Last updated 3 mins ago</Card.Text>
+              </Card.ImgOverlay>
+            </Card>
+
+            <Card className="bg-dark text-white" style={{height: "250px", width: "250px"}}>
+              <Card.Img src="./logo192.png" alt="Card image" style={{height: "250px", width: "250px"}}/>
+              <Card.ImgOverlay>
+                <Card.Title>Card title</Card.Title>
+                <Card.Text>
+                  This is a wider card with supporting text below as a natural lead-in to
+                  additional content. This content is a little bit longer.
+                </Card.Text>
+                <Card.Text>Last updated 3 mins ago</Card.Text>
+              </Card.ImgOverlay>
+            </Card>
+
+            <Card className="bg-dark text-white" style={{height: "250px", width: "250px"}}>
+              <Card.Img src="./logo192.png" alt="Card image" style={{height: "250px", width: "250px"}}/>
+              <Card.ImgOverlay>
+                <Card.Title>Card title</Card.Title>
+                <Card.Text>
+                  This is a wider card with supporting text below as a natural lead-in to
+                  additional content. This content is a little bit longer.
+                </Card.Text>
+                <Card.Text>Last updated 3 mins ago</Card.Text>
+              </Card.ImgOverlay>
+            </Card>
+
+          </CardGroup>
+        </Carousel.Item>
+
+        <Carousel.Item>
+          <CardGroup style={{display: "flex", justifyContent: "space-around", gap: "30px"}}>
+
+            <Card className="bg-dark text-white" style={{height: "250px", width: "250px"}}>
+              <Card.Img src="./logo192.png" alt="Card image" style={{height: "250px", width: "250px"}}/>
+              <Card.ImgOverlay>
+                <Card.Title>Card title</Card.Title>
+                <Card.Text>
+                  This is a wider card with supporting text below as a natural lead-in to
+                  additional content. This content is a little bit longer.
+                </Card.Text>
+                <Card.Text>Last updated 3 mins ago</Card.Text>
+              </Card.ImgOverlay>
+            </Card>
+
+            <Card className="bg-dark text-white" style={{height: "250px", width: "250px"}}>
+              <Card.Img src="./logo192.png" alt="Card image" style={{height: "250px", width: "250px"}}/>
+              <Card.ImgOverlay>
+                <Card.Title>Card title</Card.Title>
+                <Card.Text>
+                  This is a wider card with supporting text below as a natural lead-in to
+                  additional content. This content is a little bit longer.
+                </Card.Text>
+                <Card.Text>Last updated 3 mins ago</Card.Text>
+              </Card.ImgOverlay>
+            </Card>
+
+            <Card className="bg-dark text-white" style={{height: "250px", width: "250px"}}>
+              <Card.Img src="./logo192.png" alt="Card image" style={{height: "250px", width: "250px"}}/>
+              <Card.ImgOverlay>
+                <Card.Title>Card title</Card.Title>
+                <Card.Text>
+                  This is a wider card with supporting text below as a natural lead-in to
+                  additional content. This content is a little bit longer.
+                </Card.Text>
+                <Card.Text>Last updated 3 mins ago</Card.Text>
+              </Card.ImgOverlay>
+            </Card>
+
+            <Card className="bg-dark text-white" style={{height: "250px", width: "250px"}}>
+              <Card.Img src="./logo192.png" alt="Card image" style={{height: "250px", width: "250px"}}/>
+              <Card.ImgOverlay>
+                <Card.Title>Card title</Card.Title>
+                <Card.Text>
+                  This is a wider card with supporting text below as a natural lead-in to
+                  additional content. This content is a little bit longer.
+                </Card.Text>
+                <Card.Text>Last updated 3 mins ago</Card.Text>
+              </Card.ImgOverlay>
+            </Card>
+
+          </CardGroup>
+        </Carousel.Item>
+
+        <Carousel.Item>
+          <CardGroup style={{display: "flex", justifyContent: "space-around", gap: "30px"}}>
+
+          <Card className="bg-dark text-white" style={{height: "250px", width: "250px"}}>
+            <Card.Img src="./logo192.png" alt="Card image" style={{height: "250px", width: "250px"}}/>
+            <Card.ImgOverlay>
+              <Card.Title>Card title</Card.Title>
+              <Card.Text>
+                This is a wider card with supporting text below as a natural lead-in to
+                additional content. This content is a little bit longer.
+              </Card.Text>
+              <Card.Text>Last updated 3 mins ago</Card.Text>
+            </Card.ImgOverlay>
+          </Card>
+
+          <Card className="bg-dark text-white" style={{height: "250px", width: "250px"}}>
+            <Card.Img src="./logo192.png" alt="Card image" style={{height: "250px", width: "250px"}}/>
+            <Card.ImgOverlay>
+              <Card.Title>Card title</Card.Title>
+              <Card.Text>
+                This is a wider card with supporting text below as a natural lead-in to
+                additional content. This content is a little bit longer.
+              </Card.Text>
+              <Card.Text>Last updated 3 mins ago</Card.Text>
+            </Card.ImgOverlay>
+          </Card>
+
+          <Card className="bg-dark text-white" style={{height: "250px", width: "250px"}}>
+            <Card.Img src="./logo192.png" alt="Card image" style={{height: "250px", width: "250px"}}/>
+            <Card.ImgOverlay>
+              <Card.Title>Card title</Card.Title>
+              <Card.Text>
+                This is a wider card with supporting text below as a natural lead-in to
+                additional content. This content is a little bit longer.
+              </Card.Text>
+              <Card.Text>Last updated 3 mins ago</Card.Text>
+            </Card.ImgOverlay>
+          </Card>
+
+          <Card className="bg-dark text-white" style={{height: "250px", width: "250px"}}>
+            <Card.Img src="./logo192.png" alt="Card image" style={{height: "250px", width: "250px"}}/>
+            <Card.ImgOverlay>
+              <Card.Title>Card title</Card.Title>
+              <Card.Text>
+                This is a wider card with supporting text below as a natural lead-in to
+                additional content. This content is a little bit longer.
+              </Card.Text>
+              <Card.Text>Last updated 3 mins ago</Card.Text>
+            </Card.ImgOverlay>
+          </Card>
+
+          </CardGroup>
+        </Carousel.Item>
+
+      </Carousel>
+    </div>
+  );
+}
+
+export default CardsSlider;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,46 @@
+import './Header.css';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import FormControl from 'react-bootstrap/FormControl'
+
+function Header() {
+  return (
+    <div >
+      <Navbar bg="dark" expand="lg" variant="dark">
+        <Container>
+          <Navbar.Brand href="/">BookMyShow</Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse id="navbarScroll">
+            <Nav
+              className="me-auto my-2 my-lg-0"
+              style={{ maxHeight: '100px' }}
+              navbarScroll
+            >
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="/movies">All Movies</Nav.Link>
+              <Nav.Link href="/register">User Registration</Nav.Link>
+            </Nav>
+            <Form className="d-flex">
+              <FormControl
+                type="search"
+                placeholder="Search"
+                className="me-2"
+                aria-label="Search"
+              />
+              <Button variant="outline-success">Search</Button>
+            </Form>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <Container fluid>
+
+      </Container>
+    </div>
+  );
+}
+
+export default Header;
